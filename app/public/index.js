9c7d6543d21ae4f819875e1be3e07fe380551e17
aidanbdh <aidanbdh1998@gmail.com>
2019-03-07 14:47:14 -0800

    Added larger buttons and text for mobile devices #18

--- app/public/index.js
+++ app/public/index.js
@@ -38,16 +38,36 @@ const $hideAnswer = document.getElementById('hide-answer')
 $showAnswer.addEventListener('click', () => {
     $answer.textContent = puzzle.answer
 
-    $showAnswer.classList = 'hidden'
-    $answer.classList = ''
-    $hideAnswer.classList = ''
+    $showAnswer.classList.add('hidden')
+    $answer.classList.remove('hidden')
+    $hideAnswer.classList.remove('hidden')
 })
 
 $hideAnswer.addEventListener('click', () => {
-    $answer.classList = 'hidden'
-    $hideAnswer.classList = 'hidden'
-    $showAnswer.classList = ''
+    $answer.classList.add('hidden')
+    $hideAnswer.classList.add('hidden')
+    $showAnswer.classList.remove('hidden')
 })
+
+console.log('Width: ' + window.outerWidth)
+
+const $body = document.getElementsByTagName('body')[0]
+const $buttons = document.getElementsByTagName('button')
+const $inputs = document.getElementsByTagName('input')
+
+if(window.outerWidth < 960) {
+    $body.classList.add('mobile')
+
+    console.log($buttons)
+    for(let i = 0; i < $buttons.length; i++) {
+        console.log($buttons[i])
+        $buttons[i].classList.add('mobile')
+    }
+
+    for (let i = 0; i < $inputs.length; i++) {
+        $inputs[i].classList.add('mobile')
+    }
+} 
 
 // This function displays the puzzle along with the difficulty.
 function displayPuzzle() {
@@ -61,13 +81,13 @@ function displayPuzzle() {
     $puzzle.textContent = str
 
     $answerInput.value = ''
-    $results.classList = 'hidden'
+    $results.classList.add('hidden')
 
     $answer.textContent = ''
 
-    $answer.classList = 'hidden'
-    $hideAnswer.classList = 'hidden'
-    $showAnswer.classList = ''
+    $answer.classList.add('hidden')
+    $hideAnswer.classList.add('hidden')
+    $showAnswer.classList.remove('hidden')
 }
 
 displayPuzzle()
